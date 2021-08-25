9a62cef716134cc08e11392197a47fb83edb0cdf
RishikeshTbr <tbr30929@gmail.com>
2021-08-25 12:26:07 +0530

    created input component and installed new library called react-icons

--- src/components/BrandInput/index.js
+++ src/components/BrandInput/index.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
+import { AiOutlineSearch } from "react-icons/ai";
 
 export const BrandInput = () => {
-    return (
-        <div>
-            <h1>BrandInput</h1>
-            
-        </div>
-    )
-}
+  return (
+    <div>
+      <div className="flex px-3 py-3 align-middle shadow-md w-96 rounded">
+        <AiOutlineSearch className="mx-5 self-center text-xl text-gray-darkGrayLMI" />
+        <input
+          className="border-0 outline-none h-3 w-72 py-3"
+          type="text"
+          placeholder="Search for a country"
+        />
+      </div>
+    </div>
+  );
+};
