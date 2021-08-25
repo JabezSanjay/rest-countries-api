5524527d9e713b1595c804c5e0e0124d32b60aaf
JabezSanjay <jbzsanjay2000@gmail.com>
2021-08-25 10:37:23 +0530

    Basic template for the project

---
+++ tailwind.config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  purge: ["./src/**/*.{js,jsx,ts,tsx}", "./public/index.html"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors: {
+      blue: {
+        dark: "hsl(209, 23%, 22%)",
+        veryDarkDMB: "hsl(207, 26%, 17%)",
+        veryDarkLMT: "hsl(200, 15%, 8%)",
+      },
+      white: {
+        default: "hsl(0, 0%, 100%)",
+      },
+      gray: {
+        darkGrayLMI: "hsl(0, 0%, 52%)",
+        veryLightGrayLMB: "hsl(0, 0%, 98%)",
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
