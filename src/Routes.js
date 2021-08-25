bdbf4fa1b6b63076eb2a2bb628f41e44ef18f764
JabezSanjay <jbzsanjay2000@gmail.com>
2021-08-25 12:25:28 +0530

    Completed BrandNavbar

--- src/Routes.js
+++ src/Routes.js
@@ -1,10 +1,16 @@
 import React from "react";
 import { BrandInput } from "./components/BrandInput";
+import BrandNavbar from "./components/BrandNavbar";
 
 const Routes = () => {
   return (
     <div>
-      <div><BrandInput/></div>
+      <div>
+        <BrandInput />
+      </div>
+      <div>
+        <BrandNavbar />
+      </div>
     </div>
   );
 };
---
+++ src/components/BrandNavbar/index.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { HiOutlineMoon } from "react-icons/hi";
+
+const BrandNavbar = () => {
+  return (
+    <div>
+      <nav className="flex justify-between bg-white-default shadow p-6">
+        <div>
+          <a
+            href="/"
+            className="text-blue-veryDarkLMT font-extrabold text-2xl ml-10"
+          >
+            Where in the world?
+          </a>
+        </div>
+        <div className="text-lg self-center">
+          <button className="flex">
+            <HiOutlineMoon className="self-center mr-2" />
+            <h6 className="text-blue-veryDarkLMT font-semibold mr-20">
+              Dark Mode
+            </h6>
+          </button>
+        </div>
+      </nav>
+    </div>
+  );
+};
+
+export default BrandNavbar;
