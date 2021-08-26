65aefc3d40e0160705c25c3a8c63df49e0d4b58e
JabezSanjay <jbzsanjay2000@gmail.com>
2021-08-26 14:54:27 +0530

    Design fixes in card

--- src/components/HomePageCard/index.js
+++ src/components/HomePageCard/index.js
@@ -2,15 +2,15 @@ import React from "react";
 
 const HomePageCard = () => {
   return (
-    <div className="shadow-md w-48 bg-white-default m-auto rounded-md">
+    <div className="shadow-md max-w-xs w-64 bg-white-default m-auto rounded-md">
       <img
         src="https://restcountries.eu/data/umi.svg"
         alt="error"
-        className="rounded-t-md"
+        className="rounded-t-md h-36"
       />
-      <div className="text-sm pb-5 pt-3 px-4" style={{ fontSize: "9px" }}>
-        <h1 className="font-extrabold mb-1" style={{ fontSize: "12px" }}>
-          Uniited States of America
+      <div className="text-sm pb-10 pt-3 px-4" style={{ fontSize: "0.8rem" }}>
+        <h1 className="font-extrabold my-3" style={{ fontSize: "1.1rem" }}>
+          United States of America
         </h1>
         <p>
           <span className="font-bold">Population: </span>323,947,000
