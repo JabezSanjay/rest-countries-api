ea8049ba6b710976c58f776f271b63fa7aa48e84
RishikeshTbr <tbr30929@gmail.com>
2021-08-30 10:16:43 +0530

    installed a pakage

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import Routes from "./Routes";
+import { store } from "./redux/store";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
